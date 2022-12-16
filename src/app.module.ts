d9ff10b145c8b0743871f7e59e90413558894a55
Gabriel Buzzi Venturi <gabrielbuzziv@gmail.com>
2022-12-16 10:24:17 -0300

    chore: add graphql example

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ExampleResolver } from './example.resolver';
+
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      // plugins: [new ComplexityPlugin(20)],
+      cors: { origin: true },
+    }),
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, ExampleResolver],
 })
 export class AppModule {}
